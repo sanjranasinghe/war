f98adfd9a2c67e7621d433bcce80272ea1b17368
root <root@localhost.localdomain>
2021-07-06 01:04:07 -0400

    first commit

---
+++ src/main/java/com/example/sanjaya/SanjayaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sanjaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanjayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanjayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sanjaya/SanjayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanjaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanjayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
